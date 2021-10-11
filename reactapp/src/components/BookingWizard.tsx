6fd55db72acc055c6cc16eee6ccab14d8c449d59
Leo Childs <leo.childs@live.com>
2021-10-11 18:09:07 +0100

    appointment types

--- reactapp/src/components/BookingWizard.tsx
+++ reactapp/src/components/BookingWizard.tsx
@@ -4,28 +4,37 @@ import { Councellor } from '../data/councellor-data';
 
 export class BookingWizard extends react.Component<any, any>{
     councellorAppointmentData: CouncellorAppointmentData;
+    availableTypes: string[];
     constructor(props: { [key: string]: any }) {
         super(props);
         this.councellorAppointmentData = new CouncellorAppointmentData();
+        this.availableTypes = CouncellorAppointmentData.getAppointmentTypes();
         this.state = {
             selectedDate: '2021-08-17',
             selectedTime: '11:00',
-            availableCouncellors: this.councellorAppointmentData.getAvailableCouncellors('2021-08-17', '11:00'),
-            availableTimes: this.councellorAppointmentData.getAvailableAppointmentTimes('2021-08-17')
+            selectedType: 'one_off',
+            availableCouncellors: this.councellorAppointmentData.getAvailableCouncellors('one_off', '2021-08-17', '11:00'),
+            availableTimes: this.councellorAppointmentData.getAvailableAppointmentTimes('one_off', '2021-08-17')
         }
         this.handleChangeDate = this.handleChangeDate.bind(this);
+        this.handleChangeTime = this.handleChangeTime.bind(this);
         this.handleChangeTime = this.handleChangeTime.bind(this);
     }
     handleChangeDate(event: { target: { value: any; }; }) {
         this.setState({
             selectedDate: event.target.value,
-            availableTimes: this.councellorAppointmentData.getAvailableAppointmentTimes(event.target.value)
+            availableTimes: this.councellorAppointmentData.getAvailableAppointmentTimes(this.state.selectedType, event.target.value)
         });
     }
     handleChangeTime(time: string) {
         this.setState({
             selectedTime: time,
-            availableCouncellors: this.councellorAppointmentData.getAvailableCouncellors(this.state.selectedDate, time),
+            availableCouncellors: this.councellorAppointmentData.getAvailableCouncellors(this.state.selectedType, this.state.selectedDate, time),
+        })
+    }
+    handleTypeSelection(type: string) {
+        this.setState({
+            selectedType: type
         })
     }
     render() {
@@ -34,6 +43,9 @@ export class BookingWizard extends react.Component<any, any>{
         });
         const renderAvailableTimes = this.state.availableTimes.map((time: string) => {
             return <a className='w3-btn' onClick={() => this.handleChangeTime(time)}>{time}</a>
+        });
+        const renderTypes = this.availableTypes.map((type: string) => {
+            return <a className='w3-btn' onClick={() => this.handleTypeSelection(type)}>{type.replace(/_/i, '-')}</a>
         })
         return (
             <form className="w3-container">
@@ -43,7 +55,7 @@ export class BookingWizard extends react.Component<any, any>{
                 </section>
                 <section>
                     <h1>Select Type of Appointment</h1>
-
+                    {renderTypes}
                 </section>
                 <section>
                     <h1>Select date</h1>
--- reactapp/src/data/councellors.ts
+++ reactapp/src/data/councellors.ts
@@ -23,20 +23,23 @@ export class CouncellorAppointmentData {
         const mediums = [...new Set(councellors.map(c => c.appointment_mediums).flat())];
         return mediums;
     }
-    getAvailableAppointmentTimes(date: string): string[] {
-        const allTimes = this.councellorAvailability.flatMap(cA => cA.availability!.flatMap(a => a.datetime));
+
+    getAvailableAppointmentTimes(type: string, date: string): string[] {
+        const allTimes = this.councellorAvailability
+            .filter(c => c.appointment_types.some(t => t === type))
+            .flatMap(cA => cA.availability!.flatMap(a => a.datetime));
         const filteredByDate = allTimes.filter(t => t.includes(date));
         const unique = [...new Set(filteredByDate)];
         const times = unique.map(u => u.substring(11, 16)).sort();
         return times;
     }
-    getAvailableCouncellors(date: string, time: string): Councellor[] {
+    getAvailableCouncellors(type: string, date: string, time: string): Councellor[] {
         // Data string must confirm to HTML date input YYYY-MM-DD
         // TODO: Check correct format and make a fuss if its wrong
         const councellorList = this.councellorAvailability.filter((c: Councellor) => {
-            return c.availability!.some(a => a.datetime.includes(`${date}T${time}`));
+            return c.appointment_types.some(t => t === type) && c.availability!.some(a => a.datetime.includes(`${date}T${time}`));
         })
-        console.info(councellorList);
+
         return councellorList;
     }
 }
